164c5765717eef14ddff44cdad72190bf8519e5d
RMDircio <reginadircio@gmail.com>
2020-12-25 17:16:58 -0600

    Parent and Child Classes

--- Python/OOP_Raza_Hussain/OOP.py
+++ Python/OOP_Raza_Hussain/OOP.py
@@ -4,20 +4,20 @@
 ##        and Classes with Details     ## 
 #---------------------------------------#
 
-# # syntax of classes
-# class myself():
-#     pass
+# syntax of classes
+class myself():
+    pass
 
-# # Instance Attribute - that will execute whenever we instantiate the class
-# class Cat():
-#     species = 'animal'
+# Instance Attribute - that will execute whenever we instantiate the class
+class Cat():
+    species = 'animal'
 
-#     def __init__(self): # Constructor method that will execute as soon as the class is instantiated
-#         print('Nice, we have a cat.')
+    def __init__(self): # Constructor method that will execute as soon as the class is instantiated
+        print('Nice, we have a cat.')
 
-# Tom = Cat()
-# print(Tom)
-# print(Tom.species)
+Tom = Cat()
+print(Tom)
+print(Tom.species)
 
 # ''' -------------------------------------------------------------- '''
 
@@ -71,6 +71,81 @@ print('Does "white" equal "white2"? -->', white == white2)
 # access the class attributes
 print(f'Blue is a {blu.species}')
 print(f'Woo is a {woo.species}')
+
+print('--------------------------------------------')
+
+
+
+#---------------------------------------#
+##      Inheritance, Parents and       ##
+##        Child Class the Class        ##
+##      and Use of Super Functions     ## 
+#---------------------------------------# 
+
+# parent class
+class Bird:
+    color = 'red'
+    def __init__(self):
+        print('Bird is ready')
+
+    def whoisthis(self):
+        print('Bird')
+    
+    def swim(self):
+        print('Swim faster')
+
+# child class
+class Penguin(Bird):
+    def __init__(self):
+        # use super() function
+        super().__init__() # calls upon parent class
+        print('Penguin is ready')
+    
+    # overwrites parent class
+    def whoisthis(self):
+        print('Penguin')
+
+    def run(self):
+        print('Run faster')
+
+peggy = Penguin()
+print(peggy.whoisthis())
+print(peggy.swim())
+print(peggy.run())
+print('-----------------------------')
+jerry = Bird()
+print(jerry.whoisthis())
+
+# you can override the Parent class within the Child class - redefining it
+class SomeOtherBird(Bird):
+    color = 'White'
+
+print('----------------------------')
+newbird = SomeOtherBird()
+print(newbird.color)
+
+''' Trying on my own '''
+
+# make a parent animal class
+
+class Animal():
+    legs = 4
+    def __init__(self):
+        print('This is from the Animal Class')
+    
+class Hawk(Animal):
+    legs = 2
+    def __init__(self):
+        super().__init__()
+        print('This is from the Hawk Class')
+
+print('----------------------------')
+greg = Animal()
+print(greg.legs)
+
+hank = Hawk()
+print(hank.legs)
+
 
 
 
