7a091e90532ca69d77cedf29da114cbf6970d0c4
RMDircio <reginadircio@gmail.com>
2021-02-08 12:23:31 -0600

    Python One Exercise Complete

--- Python/Flask_Bootcamp_Portilla/Flask-Bootcamp-master/01-Python/Python_Level_One/05-Exercise_Review.py
+++ Python/Flask_Bootcamp_Portilla/Flask-Bootcamp-master/01-Python/Python_Level_One/05-Exercise_Review.py
@@ -14,17 +14,22 @@ s = 'flask'
 
 # Use indexing to print out the following:
 # 'f'
+print(s[0])
 
 # 's'
+print(s[-2])
 
 # 'ask'
+print(s[2:])
 
 # 'las'
+print(s[1:4])
 
 # 'k'
+print(s[-1])
 
 # Bonus: Use indexing to reverse the string
-
+print(s[::-1])
 
 ###############
 ## Problem 2 ##
@@ -32,8 +37,10 @@ s = 'flask'
 
 # Given this nested list:
 mylist = [3,7,[1,4,'hello']]
-# Reassign "hello" to be "goodbye"
 
+# Reassign "hello" to be "goodbye"
+mylist[2][-1] = 'goodbye'
+print(mylist)
 
 ###############
 ## Problem 3 ##
@@ -42,11 +49,13 @@ mylist = [3,7,[1,4,'hello']]
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
 d1 = {'simple_key':'hello'}
+print(d1['simple_key'])
 
 d2 = {'k1':{'k2':'hello'}}
+print(d2['k1']['k2'])
 
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
-
+print(d3['k1'][0]['nest_key'][-1][0])
 
 ###############
 ## Problem 4 ##
@@ -54,7 +63,7 @@ d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
 # Use a set to find the unique values of the list below:
 mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
-
+print(set(mylist))
 
 ###############
 ## Problem 5 ##
@@ -66,3 +75,4 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
+print(f"Hello my dog's name is {name} and he is {age} years old")
