440fe3504e22fd8c4fdf8f8e4216e7e0ea8d15ad
RMDircio <reginadircio@gmail.com>
2021-03-15 17:10:13 -0500

    Routing Exercise Complete :tada:

--- Python/Flask_Bootcamp_Portilla/Flask-Bootcamp-master/02-Flask-Basics/05-Routing_Exercise.py
+++ Python/Flask_Bootcamp_Portilla/Flask-Bootcamp-master/02-Flask-Basics/05-Routing_Exercise.py
@@ -1,22 +1,36 @@
 # Set up your imports here!
-# import ...
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
 
-@app.route('') # Fill this in!
+@app.route('/') # Fill this in!
 def index():
-    # Welcome Page
     # Create a generic welcome page.
-    pass
+    return '<h1>Hello Puppers!</h1>'
+    
 
-@app.route('') # Fill this in!
+@app.route('/latin/<name>') # Fill this in!
 def puppylatin(name):
     # This function will take in the name passed
     # and then use "puppy-latin" to convert it!
 
     # HINT: Use indexing and concatenation of strings
     # For Example: "hello"+" world" --> "hello world"
-    pass
+    
+    # if the name does end with 'y'
+    if name[-1] == 'y':
+        # remove the 'y' + add 'iful' to the name
+        changed_name = name[:-1] + 'iful'
+        return 'Your new name is {}'.format(changed_name)
+        
+    
+    # if the name does not end with 'y'
+    else:
+        # add an 'y' to the end of the name
+        changed_name = name + 'y'
+        return 'Your new name is {}'.format(changed_name)
+
 
 if __name__ == '__main__':
-    # Fill me in!
-    pass
+    app.run(debug=True)
